6a9b037acd55bdc7e04e7f9e4fe9115eea2dbb69
fedaiipek <fedaiipek55@gmail.com>
2021-01-27 18:26:39 -0600

    movie 1 branch working here

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning Git");
+        System.out.println("Watching more movie in bedroom");
 
     }
 
