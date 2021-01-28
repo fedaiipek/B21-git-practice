7ff42c287dcffd74581c0212991e9280394e523d
fedaiipek <fedaiipek55@gmail.com>
2021-01-27 19:30:43 -0600

    movie 2 branch watchng on the phone 10 minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, B22");
+        System.out.println("Hello World, B21");
         System.out.println("B21 future SDET learning Git");
-        System.out.println("Ready to go back to living room");
+       //movie on the phone 10 minutes
 
     }
 
